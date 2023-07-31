a0de5f7463ba9e74f497023080a9dbddb0114ecf
Achira Wijesuriya <129876238+arwijesuriya@users.noreply.github.com>
2023-07-31 23:15:36 +0530

    Add files via upload

---
+++ cpp-02.cpp
@@ -0,0 +1,16 @@
+/*
+    Date: 12.10.2022
+    Practical Number: 02
+    Problem Specification: Write a program to display name, registration number, and address
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    cout<<"Mahinda Rajapakse"<<endl;
+    cout<<"2004/TSUNAMI/001"<<endl;
+    cout<<"No: 01, 'Carlton House', Mahawela Road, Tangalle"<<endl;
+
+    return 0;
+}
---
+++ cpp-07.cpp
@@ -0,0 +1,23 @@
+/*
+    Date: 14.10.2022
+    Practical Number: 07
+    Problem Specification: Write a simple program to do both declaration & initialization in same line
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    //Declaration and Initialization in same line
+    string name = "M.R.Rajapakse";
+    int age = 75;
+    string address = "Tangalle";
+    char sex = 'P';
+
+    cout<<"Name: "<<name<<endl;
+    cout<<"Age: "<<age<<endl;
+    cout<<"Address: "<<address<<endl;
+    cout<<"Sex: "<<sex<<endl;
+
+    return 0;
+}
